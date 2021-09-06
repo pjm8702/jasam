31d15e7e0dfa7545caec6c00381a73916a92a24e
pjm8702 <pjm8702@gmail.com>
2021-09-06 21:46:03 +0900

    개인정보 txt 파일에서 비밀번호 읽기 개선

--- Autorun.py
+++ Autorun.py
@@ -3,6 +3,10 @@ from pywinauto import timings
 import time
 import os
 
+f = open("bungaeInfo.txt", "r")
+pw = f.readline().rstrip()
+authorize_pw = f.readline().rstrip()
+
 app = application.Application()
 app.start("C:\\KiwoomFlash3\\Bin\\NKMiniStarter.exe")
 
@@ -11,11 +15,11 @@ dlg = timings.WaitUntilPasses(20, 0.5, lambda: app.window_(title=title))
 
 pass_ctrl = dlg.Edit2
 pass_ctrl.SetFocus()
-pass_ctrl.TypeKeys('Kiwoom Login Password')
+pass_ctrl.TypeKeys(pw)
 
 cert_ctrl = dlg.Edit3
 cert_ctrl.SetFocus()
-cert_ctrl.TypeKeys('공인인증서 Password')
+cert_ctrl.TypeKeys(authorize_pw)
 
 btn_ctrl = dlg.Button0
 btn_ctrl.Click()
