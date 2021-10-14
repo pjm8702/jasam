ccedfd4546d4ee86b98703423709226e0d91d947
pjm8702 <57159383+pjm8702@users.noreply.github.com>
2021-10-14 18:34:40 +0900

    Update [Lv2]GearShift.c

--- Softeer/[Lv2]GearShift.c
+++ Softeer/[Lv2]GearShift.c
@@ -1,3 +1,21 @@
+/*
+[Lv2]GearShift.c 
+1단에서 8단으로 연속적으로 변속을 한다면 ascending, 8단에서 1단으로 연속적으로 변속한다면 descending, 둘다 아니라면 mixed 라고 정의했다. 
+순서가 주어졌을 때 이것이 ascending인지, descending인지, 아니면 mixed인지 출력하는 프로그램을 작성하시오.
+
+[입력형식] 
+첫째 줄에 8개 숫자가 주어진다. 1부터 8까지 숫자가 한번씩 등장한다.
+
+[출력형식] 
+첫째 줄에 ascending, descending, mixed 중 하나를 출력한다.
+
+[입력예제] 
+1 2 3 4 5 6 7 8
+
+[출력예제] 
+ascending
+*/
+
 #include <stdio.h>
 
 
