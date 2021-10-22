96f8659936948f7f343944518a87c3fea45e1a6a
pjm8702 <57159383+pjm8702@users.noreply.github.com>
2021-10-22 12:11:09 +0900

    Create simple_print.c

---
+++ Acmicpc/Step1/simple_print.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("%s\n\n", "Hello World!\n");
+  
+  printf("%s\n", "강한친구 대한육군");
+  printf("%s\n\n", "강한친구 대한육군");
+  
+  printf("%s\n", "\\    /\\");
+  printf("%s\n", " )  ( ')");
+  printf("%s\n", "(  /  )");
+  printf("%s\n\n", " \\(__)|");
+  
+  printf("%s\n", "|\\_/|");
+  printf("%s\n", "|q p|   /}");
+  printf("%s\n", "( 0 )\"\"\"\\");
+  printf("%s\n", "|\"^\"`    |");
+  printf("%s\n", "||_/=\\\\__|");
+  
+  return 0;
+}
