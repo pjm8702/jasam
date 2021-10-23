7d429548852438ef5e530de0ff6a89f4d39dcaee
Jasam <pjm8702@gmail.com>
2021-10-23 20:05:52 +0900

    create simple_calculation.c

---
+++ Acmicpc/Step1/simple_calculation.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+    
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n", a+b);
+    printf("%d\n", a-b);
+    printf("%d\n", a*b);
+    //printf("%.9lf\n", (double)a/b);
+    printf("%d\n", a/b);
+    printf("%d\n", a%b);
+
+    printf("%d\n", (a+b)%c);
+    printf("%d\n", ((a%c)+(b%c))%c);
+    printf("%d\n", (a*b)%c);
+    printf("%d\n", ((a%c)*(b%c))%c);
+
+    return 0;
+}
