a83b462e7b96e646f0efa2de67199126da8f6129
Jasam <pjm8702@gmail.com>
2021-10-23 21:17:15 +0900

    modify case_a_plus_b.c

--- Acmicpc/Step3/case_a_plus_b.c
+++ Acmicpc/Step3/case_a_plus_b.c
@@ -8,7 +8,8 @@ int main(void)
     for(i = 0; i < N; i++)
     {
         scanf("%d %d", &a, &b);
-        printf("Case #%d: %d\n", i + 1, a + b);
+        //printf("Case #%d: %d\n", i + 1, a + b);
+        printf("Case #%d: %d + %d = %d\n", i + 1, a, b, a + b);
     }
     return 0;
 }
