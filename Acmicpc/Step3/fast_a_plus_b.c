602d96ebd5b45e43562b16c6a00aca4de622f545
Jasam <pjm8702@gmail.com>
2021-10-23 21:05:42 +0900

    create fast_a_plus_b.c

---
+++ Acmicpc/Step3/fast_a_plus_b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int T, a, b, i;
+
+    scanf("%d", &T);
+    for(i = 0; i < T; i++)
+    {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+
+    return 0;
+}
